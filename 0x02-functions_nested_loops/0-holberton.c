fdca2cc06798b63b1ec3b31b90ac2e5c8c451995
Nedudigital <segunchinedu2000@gamil.com>
2022-03-16 00:16:00 +0100

    nested_loops

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,18 +1,24 @@
 #include "main.h"
 
 /**
- * main - debugging example
- * Return: 0
+ * main - prints Putchar
+ *
+ * Description: prints _putchar
+ *
+ * Return: 0 (Success)
  */
+
 int main(void)
 {
-	char *holberton = "Holberton";
-
-	while (*holberton)
-	{
-		_putchar(*holberton);
-		holberton++;
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }

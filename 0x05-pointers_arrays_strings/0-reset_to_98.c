13a5c33a88bf6e33274c1d15e1cb1b55febb6af5
Nedudigital <segunchinedu2000@gamil.com>
2022-03-21 23:32:56 +0100

    pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h" 
+
+/**
+ * @brief *reset_to_98 - updates the value *n and points it to the value 98
+ * @n: the number to modify
+ */
+
+void reset_to_98(int *n)
+{
+    *n = 98;
+}
